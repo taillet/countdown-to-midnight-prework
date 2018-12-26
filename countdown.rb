97b1619331fd70ac8b97887d7b63efe53940a47d
Heloise Taillet <ht2394@columbia.edu>
2018-12-26 04:49:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,12 @@
 
 
 def countdown(number)
-  while number !=0
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
+  puts "HAPPY NEW YEAR!"
+
 end
 
 def countdown_with_sleep(number)
